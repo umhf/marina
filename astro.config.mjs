9a30081de8f6b6744df4b22067c6bcee7cf8ce64
Uli Schmid <tech@trbo.com>
2022-10-18 11:11:53 +0200

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import { astroImageTools } from "astro-imagetools";
 
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools]
+  integrations: [astroImageTools, tailwind()]
 });
-
