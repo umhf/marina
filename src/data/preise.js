7ea728b956bec5ba0828a8ba6bab3e7e4d311144
Uli Schmid <tech@trbo.com>
2022-10-31 20:55:04 +0100

    add tabs

---
+++ src/data/preise.js
@@ -0,0 +1,82 @@
+export const preise = [
+   [
+            {
+                headline: "Light-Shooting",
+                subHeadline: "3 Bilder",
+                price: "185",
+                features: [
+                    "3 liebevoll und aufwendig bearbeitete Bilder",
+                    "jedes weitere Bild +25.00 EUR",
+                    "Anfahrt bis 25km von Peiting inklusive",
+                    "ca. 1,5 - 2 Stunden Shootingzeit",
+                ],
+                animal: "horse"
+            },
+            {
+                dark: true,
+                headline: "Basic-Shooting",
+                subHeadline: "8 Bilder",
+                price: "285",
+                features: [
+                    "8 liebevoll und aufwendig bearbeitete Bilder",
+                    "jedes weitere Bild +25.00 EUR",
+                    "Anfahrt bis 50km von Peiting inklusive",
+                    "ca. 2 - 3 Stunden Shootingzeit",
+                ],
+                animal: "horse"
+            },
+            {
+                headline: "Double-Shooting",
+                subHeadline: "20 Bilder",
+                price: "499",
+                features: [
+                    "Teile das Shooting mit deiner Freundin oder deinem Freund (249,50 EUR pro Person für 10 Bilder, gleiche Location)",
+                    "20 liebevoll und aufwendig bearbeitete Bilder insgesamt",
+                    "jedes weitere Bild +25.00 EUR",
+                    "Anfahrt bis 75km von Peiting inklusive",
+                    "ca. 3 Stunden Shootingzeit",
+                ],
+                animal: "horse"
+            }]
+   ,
+        [
+            {
+                headline: "Light-Shooting",
+                subHeadline: "3 Bilder",
+                price: "85",
+                features: [
+                    "3 liebevoll und aufwendig bearbeitete Bilder",
+                    "jedes weitere Bild +25.00 EUR",
+                    "Anfahrt bis 25km von Peiting inklusive",
+                    "ca. 1,5 - 2 Stunden Shootingzeit",
+                ],
+                animal: "dog"
+            },
+            {
+                dark: true,
+                headline: "Basic-Shooting",
+                subHeadline: "8 Bilder",
+                price: "285",
+                features: [
+                    "8 liebevoll und aufwendig bearbeitete Bilder",
+                    "jedes weitere Bild +25.00 EUR",
+                    "Anfahrt bis 50km von Peiting inklusive",
+                    "ca. 2 - 3 Stunden Shootingzeit",
+                ],
+                animal: "dog"
+            },
+            {
+                headline: "Double-Shooting",
+                subHeadline: "20 Bilder",
+                price: "399",
+                features: [
+                    "Teile das Shooting mit deiner Freundin oder deinem Freund (249,50 EUR pro Person für 10 Bilder, gleiche Location)",
+                    "20 liebevoll und aufwendig bearbeitete Bilder insgesamt",
+                    "jedes weitere Bild +25.00 EUR",
+                    "Anfahrt bis 75km von Peiting inklusive",
+                    "ca. 2 Stunden Shootingzeit",
+                ],
+                animal: "dog"
+            },
+    ]
+];
