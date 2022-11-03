06aae88a1c017eca030e083f276ebe8aed73359d
umhf <uli@maruli.org>
2022-11-03 07:40:06 +0100

    adjust cards home

---
+++ src/data/cards.js
@@ -0,0 +1,26 @@
+export const cards = [
+    {
+      src: "/src/images/passion_background.png",
+      headline: "A strong passion",
+      text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
+              my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
+              at what you get with my photo shooting service:`,
+      ctaText: "Mein Portfolio",
+    },
+    {
+      src: "/src/images/passion_background.png",
+      headline: "Informationen",
+      text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
+              my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
+              at what you get with my photo shooting service:`,
+      ctaText: "Informationen",
+    },
+    {
+      src: "/src/images/passion_background.png",
+      headline: "Kontakt",
+      text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
+              my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
+              at what you get with my photo shooting service:`,
+      ctaText: "Kontakt",
+    },
+  ];
