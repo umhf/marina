207a03c07e1b7af67408ddb0ba184ad5fe22b5b2
Uli Schmid <tech@trbo.com>
2022-11-04 11:16:44 +0100

    responsiveness

--- src/data/cards.js
+++ src/data/cards.js
@@ -11,8 +11,7 @@ export const cards = [
       src: "/src/images/passion_background.png",
       headline: "Informationen",
       text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
-              my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
-              at what you get with my photo shooting service:`,
+              my deep, genuine, undying love:`,
       ctaText: "Informationen",
     },
     {
