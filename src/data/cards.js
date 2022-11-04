cdd158e81585092bd2e763a5093658cb9d797e84
Uli Schmid <tech@trbo.com>
2022-11-04 16:12:02 +0100

    haufen zeug

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,25 +1,25 @@
 export const cards = [
     {
-      src: "/src/images/passion_background.png",
       headline: "A strong passion",
       text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
               my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
               at what you get with my photo shooting service:`,
-      ctaText: "Mein Portfolio",
+    ctaText: "Mein Portfolio",
+      link: "/portfolio"
     },
     {
-      src: "/src/images/passion_background.png",
       headline: "Informationen",
       text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
               my deep, genuine, undying love:`,
       ctaText: "Informationen",
+      link: "/informationen"
     },
     {
-      src: "/src/images/passion_background.png",
       headline: "Kontakt",
       text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
               my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
               at what you get with my photo shooting service:`,
       ctaText: "Kontakt",
+      link: "/kontakt"
     },
   ];
