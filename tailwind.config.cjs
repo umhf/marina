3c2a0eea330aa5457fc110cbcfc387eee579a529
umhf <uli@maruli.org>
2022-11-04 11:16:44 +0100

    responsiveness

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
 	plugins: [require('@tailwindcss/forms'),plugin(function({ addBase, theme, addComponents }) {
 		addBase({
 		  'h2': { marginBottom: theme("margin.2"), marginTop: theme("margin.4") },
-		  'h1': { fontSize: theme('fontSize.4xl'), fontFamily: theme("fontFamily.heading")},
+		  'h1': { fontSize: theme('fontSize.4xl'), fontFamily: theme("fontFamily.heading"), marginTop: theme("margin.16"), marginBottom: theme("margin.8")},
 		  "html": { fontFamily: theme("fontFamily.serif"), color: theme("colors.white.800") }, 
 		  "p": {marginBottom: theme("margin.1")}
 		}),
