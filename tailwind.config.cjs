0e80df7591092918d02b04e976c196ee5bc16c56
Uli Schmid <tech@trbo.com>
2022-11-05 13:11:15 +0100

    finish website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,15 +21,13 @@ module.exports = {
 	},
 	plugins: [require('@tailwindcss/forms'),plugin(function({ addBase, theme, addComponents }) {
 		addBase({
-		  'h2': { marginBottom: theme("margin.2"), marginTop: theme("margin.4") },
+		  'h2': { marginBottom: theme("margin.2"), marginTop: theme("margin.4"), fontSize: theme('fontSize.2xl') },
 		  'h1': { fontSize: theme('fontSize.4xl'), fontFamily: theme("fontFamily.heading"), marginTop: theme("margin.16"), marginBottom: theme("margin.8")},
 		  "html": { fontFamily: theme("fontFamily.serif"), color: theme("colors.white.800") }, 
 		  "p": {marginBottom: theme("margin.1")}
 		}),
 		addComponents({
 			".section": { "margin-top": theme("margin.8") },
-			".legal h1": {fontSize: "24px", fontFamily: theme("fontFamily.serif")},
-			".legal h2": {fontSize: "20px", fontFamily: theme("fontFamily.serif")}
 		})
 	  })],
 }
