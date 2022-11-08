53a2e96feea3481ee57f699f883b385bca34b454
Uli Schmid <tech@trbo.com>
2022-11-08 18:18:54 +0100

    a lot of changes

--- src/data/cards.js
+++ src/data/cards.js
@@ -4,14 +4,14 @@ export const cards = [
       text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
               my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
               at what you get with my photo shooting service:`,
-    ctaText: "Mein Portfolio",
+      ctaText: "Mein Portfolio",
       link: "/portfolio"
     },
     {
       headline: "Timbo was geht",
       text: `Preise kommen wohl raus und die Shooting Infos auf die Startseite, dann kann komplett "informatinoen weg". Stattdessen dann wohl wirklich "Stories". Also, überleg da was gscheits`,
-      ctaText: "Informationen",
-      link: "/informationen"
+      ctaText: "Stories",
+      link: "/stories"
     },
     {
       headline: "Kontakt",
