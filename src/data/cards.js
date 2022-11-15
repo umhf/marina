587d003463b0c065b3a2d96ae0dea8f00f09ebde
umhf <uli@maruli.org>
2022-11-15 17:08:34 +0100

    adjust pictures

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,24 +1,20 @@
 export const cards = [
     {
-      headline: "A strong passion",
-      text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
-              my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
-              at what you get with my photo shooting service:`,
+      headline: "Mein Portfolio",
+      text: `Mich definiert eine kraftvolle Leidenschaft für das, was ich tue. Momente einzufangen, die für die Ewigkeit sind. Werfe einen Blick darauf, was ich Dir anbiete. `,
       ctaText: "Mein Portfolio",
       link: "/portfolio"
     },
     {
-      headline: "Timbo was geht",
-      text: `Preise kommen wohl raus und die Shooting Infos auf die Startseite, dann kann komplett "informatinoen weg". Stattdessen dann wohl wirklich "Stories". Also, überleg da was gscheits`,
-      ctaText: "Stories",
+      headline: "Meine Stories",
+      text: `Jedes Shooting ist einzigartig und so individuell wie die Menschen und Tiere auf den Bildern. Schau rein und lass Dich inspirieren. Von dem, was ich mit meinen Kunden erleben durfte.`,
+      ctaText: "Meine Stories",
       link: "/stories"
     },
     {
-      headline: "Kontakt",
-      text: `A strong and powerful passion is not just part of who I am, it defines me. And that's what sets me apart -
-              my deep, genuine, undying love for what I do as a pet photographer. The visuals speak for themselves, look
-              at what you get with my photo shooting service:`,
-      ctaText: "Kontakt",
+      headline: "Mein Kontakt",
+      text: `Dir gefällt der erste Eindruck? Dann ist es höchste Zeit, sich kennenzulernen. Hier findest Du alles, wie Du mich am besten erreichst. Ich freue mich. `,
+      ctaText: "Mein Kontakt",
       link: "/kontakt"
     },
   ];
